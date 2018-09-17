b21b2b6929e18a8cb02ec23958c5065df8011e70
Bright <Paul.Bright@standardbank.co.za>
2018-09-17 09:42:49 +0200

    added more

---
+++ class2/za/co/paul/Paul.java
@@ -0,0 +1,12 @@
+package za.co.paul;
+
+
+public class Paul 
+{
+    public Paul(){
+        System.out.println("Starting Paul");
+    }
+    public call(){
+        System.out.println("paul received a call");
+    }
+}
---
+++ class2/za/co/tom/Tom.java
@@ -0,0 +1,12 @@
+package za.co.tom;
+
+
+public class Tom 
+{
+    public tom(){
+        System.out.println("Starting Tom");
+    }
+    public call(){
+        System.out.println("Tom received a call");
+    }
+}
