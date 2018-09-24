69d51e9ca9c0a00a2a8ac11ce932762273bb6af6
Paul Bright <paulbright@gmail.com>
2018-09-24 12:49:10 +0200

    lambdas

--- examprep/src/ocp/enums/Test.java
+++ examprep/src/ocp/enums/Test.java
@@ -9,6 +9,12 @@ package ocp.enums;
  *
  * @author paulbright
  */
+
+/*
+
+Note: All enums implicitly extend java.lang.Enum. Because a class can only extend one parent (see Declaring Classes), the Java language does not support multiple inheritance of state (see Multiple Inheritance of State, Implementation, and Type), and therefore an enum cannot extend anything else.
+Note: The constructor for an enum type must be package-private or private access. It automatically creates the constants that are defined at the beginning of the enum body. You cannot invoke an enum constructor yourself.
+*/
 enum Flags {
     TRUE, FALSE, HELLO, BYE, OKAY;
  
--- examprep/src/ocp/lambdas/Test.java
+++ examprep/src/ocp/lambdas/Test.java
@@ -12,12 +12,23 @@ import java.util.List;
  *
  * @author paulbright
  */
+
+interface Operation {
+    //int operate(int x, int y);
+    long operate(long x, long y);
+}
 public class Test {
     public static void main(String[] args) {
+        q1();
+    }
+    public static void q2() {
         List<String> codes = Arrays.asList("1st", "2nd", "3rd", "4th");
         System.out.println(codes.stream().filter(
                 s -> s.endsWith("d")).reduce((s1, s2) -> s1 + s2));
     }
     
-    
+    public static void q1() {
+        Operation o1 = (x, y) -> x + y; 
+        System.out.println(o1.operate(5, 10));
+    }
 }
