42136dac8c786b072b7a031a6c951d1b2092c1eb
Paul Bright <paul.bright@standardbank.co.za>
2018-09-26 10:13:19 +0200

    adding all

--- pull.bat
+++ pull.bat
@@ -1,5 +1,15 @@
+
+echo "adding files" 
+
 git add --all
+
+echo "done with adding files, now doing commit" 
 
 git commit -m "adding all" 
 
+echo "commit done, now doing git pull" 
+
 git pull 
+
+
+echo "Done!" 
