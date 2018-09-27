c7ae5b1766e4fec8af8392e5b96e7a7f981683a7
Paul Bright <paul.bright@standardbank.co.za>
2018-09-27 21:40:37 +0200

    add all

--- examprep/src/examquestions/Test.java
+++ examprep/src/examquestions/Test.java
@@ -23,11 +23,49 @@ public class Test {
     }
 
     static public void main(String[] args) {
-        Test t = new Test();
+        //Test t = new Test();
         //t.print();
-        
-        q1();
-        
+
+        q4();
+
+    }
+
+    public static void q4() {
+        int[] n1 = new int[3];
+        int[] n2 = {10, 20, 30, 40, 50};
+        n1 = n2;
+        for (int x : n1) {
+            System.out.print(x + ":");
+        }
+
+    }
+
+    public static void q3() {
+        String[] s = new String[2];
+        int i = 0;
+        for (String s1 : s) {
+            s[i].concat("Element " + i);
+            i++;
+        }
+        for (i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+
+    public static void q2() {
+        byte bite = 0;
+        int a = 10;
+        float f = 100;
+        float b = 10.25f;
+        double c = 100;
+        a = (int) b;
+        b = a;
+        c = b;
+        c = a;
+
+        bite = (byte) a;
+        b = (float) c;
+
     }
 
     public static void q1() {
@@ -37,7 +75,7 @@ public class Test {
                 n[i][j] = 10;
             }
         }
-        
+
         for (int i = 0; i < n.length; i++) {
             for (int j = 0; j > n[i].length; j++) {
                 System.out.println(n[i][j]);
